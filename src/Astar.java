2373cf5d5c1eda369ad07a9d02000c30cbe32bde
j10c <blyb1739@gmail.com>
2023-11-29 16:39:27 +0800

    fix(Astar): recover to original code

--- src/Astar.java
+++ src/Astar.java
@@ -1,10 +1,10 @@
-import java.util.Vector;
+import java.util.*;
 
 /**
  * This is where you implement the A-star search algorithm.
  * It currently has a Depth-First search implementation.
- *
- * @author Li Xiao
+ * 
+ * @author 
  */
 public class Astar extends SearchMethod {
     /**
@@ -15,10 +15,10 @@ public class Astar extends SearchMethod {
         //data structures needed for the search.
         init();
     }
-
+    
     /**
      * Finds a path from start to end node.
-     *
+     * 
      * @return True when the search is ready, false if not ready.
      */
     public boolean step() {
@@ -51,28 +51,28 @@ public class Astar extends SearchMethod {
                 }
             }
         }
-
+       
         return false;
     }
-
+    
     /**
      * Returns the best node in the open list. This is where you implement
      * logic for finding the best node in the open list.
-     *
+     * 
      * @return The best node in the open list
      */
     private Node getNextToVisit() {
         //Pick first node...
         Node bestNode = open.elementAt(0);
-
+        
         return bestNode;
     }
-
+    
     /**
      * Calculates the actual cost (path length) of going from the start node
      * to the specified node. Use it when calculating the best node in the
      * open list.
-     *
+     * 
      * @param n The current node
      */
     private void calculateActualCost(Node n) {
